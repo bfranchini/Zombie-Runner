b9e81ebd70b89832fec9d254aacad93ff1908c13
Bruno Franchini <hondokitsune@gmail.com>
2017-06-11 22:23:32 -0400

    Added player spawn points. Need to test all spawn points to make sure they work ok.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //private GameObject spawnParent;
+    private Transform[] spawnPoints;
+    public bool Respawn;
+
+	// Use this for initialization
+	void Start () {
+		var spawnParent = GameObject.Find("Player Spawn Points");
+
+        if (spawnParent == null)
+        {
+            Debug.Log("Could not find spawn point parent");
+            return;
+        }
+
+        spawnPoints = spawnParent.GetComponentsInChildren<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Respawn)
+            ReSpawn();	    
+	}
+
+    private void ReSpawn()
+    {
+        Respawn = false;
+
+        if (spawnPoints.Length == 0)
+            return; 
+
+        var respawnPoint = spawnPoints[Random.Range(1, spawnPoints.Length)];
+
+        transform.position = respawnPoint.position;
+    }
+}
