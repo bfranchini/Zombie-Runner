25288f8ee7466098c78d9b8df389e03102d7d426
Bruno Franchini <hondokitsune@gmail.com>
2017-06-20 22:18:57 -0400

    Added Day/Night Cycle. Currently takes 3 minutes for a day

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour {
+
+    //minutes per second. How many minutes are in one second of the game
+    [Tooltip("Number of minutes per second that pass")]
+    public float MinutesPerSecond = 60f;
+
+    private Transform transform;       
+    private float degreesPerFrame;
+
+    // Use this for initialization
+    void Start ()
+	{
+	    transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{             
+	    degreesPerFrame = MinutesPerSecond * Time.deltaTime * 360 / 1440; //1440 minutes in a day	    	    
+	    transform.Rotate(degreesPerFrame, 0, 0, Space.Self);
+	}
+}
