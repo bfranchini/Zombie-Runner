bacc2d5e513463d1b969be7a360667a20b3f260e
Bruno Franchini <hondokitsune@gmail.com>
2017-06-25 19:05:53 -0400

    Made helicopter kinematic. Made helicopter move towards landing area. Time helicopter takes to get to landing area needs to be fixed. Maybe don't use movetowards?

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Helicopter : MonoBehaviour
 {    
     //todo: make private
-    public bool called = false;
+    private bool called;
+    public float Speed = 50f; //50f = 5 minutes
     private AudioSource audioSource;
     private Rigidbody rigidbody;
+    private GameObject landingArea;
+    private float travelTime;
 
 	// Use this for initialization
 	void Start ()
@@ -15,13 +18,39 @@ public class Helicopter : MonoBehaviour
 	    rigidbody = GetComponent<Rigidbody>();
 	}
 
+    void Update()
+    {
+        if (called)
+        {
+            var step = Speed * Time.deltaTime;
+            travelTime += Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, landingArea.transform.position, step);
+        }
+    }
+
     public void OnDispatchHelicopter()
     {
         if (!called)
-        {
+        {               
+            //find landing area
+            landingArea = GameObject.FindGameObjectWithTag("LandingArea");
+
+            if (landingArea == null)
+            {
+                Debug.LogError("Could not find landing area");
+                return;
+            }
+
             called = true;
-            Debug.Log("Helicopter called");     
-            rigidbody.velocity = new Vector3(0, 0, 50f);
+            Debug.Log("Helicopter called");
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "LandingArea")
+        {
+            Debug.Log("total flight seconds: " + travelTime);
         }
     }
 }
