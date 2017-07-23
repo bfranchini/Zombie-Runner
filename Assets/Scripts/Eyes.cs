536b2b1d3e0026173797f07c4a85ff77a4a26999
Bruno Franchini <hondokitsune@gmail.com>
2017-07-22 23:47:35 -0400

    implemented smooth zoom and remapped zoom buttons to mouse button 2 and Q

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -5,21 +5,40 @@ using UnityEngine;
 public class Eyes : MonoBehaviour
 {
 
-    private Camera eyes;
+    private Camera playerCamera;
     private float defaultFov; //field of view
+    private float zoomMultiplier = 1.5f; //how much to zoom in
+    private float zoomedFieldOfView;
+    public float zoomSpeed = 5f;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    eyes = GetComponent<Camera>();
-	    defaultFov = eyes.fieldOfView;
+	    playerCamera = GetComponent<Camera>();
+	    defaultFov = playerCamera.fieldOfView;
+	    zoomedFieldOfView = defaultFov / zoomMultiplier;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (Input.GetButton("Zoom"))     
-	        eyes.fieldOfView = defaultFov / 1.5f;	        
-	    else	    
-	        eyes.fieldOfView = defaultFov;                   
+	void Update ()
+	{
+	    var fov = playerCamera.fieldOfView;
+
+	    if (Input.GetButton("Zoom"))
+	    {
+	        if (fov > zoomedFieldOfView)
+	        {
+	            playerCamera.fieldOfView = 
+                    Mathf.Clamp(fov - zoomedFieldOfView * Time.deltaTime * zoomSpeed, zoomedFieldOfView, defaultFov);
+	        }
+
+            return;
+        }
+
+	    if (fov < defaultFov)
+	    {
+	         playerCamera.fieldOfView = 
+                Mathf.Clamp(fov + defaultFov * Time.deltaTime * zoomSpeed, 0, defaultFov); 	        
+	    }	        
 	}
 }
